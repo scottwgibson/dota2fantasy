d52708aa949d53d3013123ad4a6518d4a5361088
Scott Gibson <mail@scottwgibson.com>
2017-07-30 22:17:56 +0200

    Add rounding to table output.

--- src/client/app/components/CardRow.js
+++ src/client/app/components/CardRow.js
@@ -11,18 +11,18 @@ export const CardRow = ( { card, player, team }) => {
         <StyledRow>
             <td>{player.name}</td>
             <td>{team.name}</td>
-            <td>{card.score.kills}</td>
-            <td>{card.score.deaths}</td>
-            <td>{card.score.creep_score}</td>
-            <td>{card.score.gpm}</td>
-            <td>{card.score.towers_killed}</td>
-            <td>{card.score.roshans_killed}</td>
-            <td>{card.score.teamfight_participation}</td>
-            <td>{card.score.obs_placed}</td>
-            <td>{card.score.creeps_stacked}</td>
-            <td>{card.score.rune_pickups}</td>
-            <td>{card.score.firstblood_claimed}</td>
-            <td>{card.score.stuns}</td>
+            <td>{card.score.kills.toFixed(2)}</td>
+            <td>{card.score.deaths.toFixed(2)}</td>
+            <td>{card.score.creep_score.toFixed(2)}</td>
+            <td>{card.score.gpm.toFixed(2)}</td>
+            <td>{card.score.towers_killed.toFixed(2)}</td>
+            <td>{card.score.roshans_killed.toFixed(2)}</td>
+            <td>{card.score.teamfight_participation.toFixed(2)}</td>
+            <td>{card.score.obs_placed.toFixed(2)}</td>
+            <td>{card.score.creeps_stacked.toFixed(2)}</td>
+            <td>{card.score.rune_pickups.toFixed(2)}</td>
+            <td>{card.score.firstblood_claimed.toFixed(2)}</td>
+            <td>{card.score.stuns.toFixed(2)}</td>
         </StyledRow>
     );
 }
